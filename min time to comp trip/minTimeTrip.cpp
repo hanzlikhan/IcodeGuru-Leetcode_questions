aa632cd8d551daf72df2bf68487e066816d9a1b5
hanzlikhan <khangormani79@gmail.com>
2024-04-22 21:06:15 +0500

    init

--- min time to comp trip/minTimeTrip.cpp
+++ min time to comp trip/minTimeTrip.cpp
@@ -7,7 +7,13 @@ int main(){
     int bus3 = 0;
     for(int i=1;i<5;i++){
         bus1++;
-        bus2+=2;
-        bus3+=3;
+        if(arr[i]%2==0){
+            bus2+=2;
+        }
+        if(arr[i]%3==0){
+             bus3+=3;
+        }
     }
+    int totaltime = bus1+bus2+bus3;
+    if(totaltime==5)
 }
