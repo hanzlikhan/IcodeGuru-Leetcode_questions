15588bd7393b0f99d9302370d4820ba4553d7547
hanzlikhan <khangormani79@gmail.com>
2024-04-22 21:00:22 +0500

    init

---
+++ min time to comp trip/minTimeTrip.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int arr[1,2,3,4,5];
+    int bus1 = 0;
+    int bus2 = 0;
+    int bus3 = 0;
+    for(int i=1;i<5;i++){
+        bus1++;
+        bus2+=2;
+        bus3+=3;
+    }
+}
