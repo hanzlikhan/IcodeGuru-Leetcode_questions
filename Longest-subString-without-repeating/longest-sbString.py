0252fa88859355b2e844e63f92fdab08a9a418cc
hanzlikhan <khangormani79@gmail.com>
2024-05-04 14:48:41 +0500

    init

--- Longest-subString-without-repeating/longest-sbString.py
+++ Longest-subString-without-repeating/longest-sbString.py
@@ -1,12 +1,35 @@
-n = len(s)
-lgS = 0
-left = 0
-seen = set()
-for right in range(len(s)):
-    if s[right] not in seen:
-        seen.add(s[right])
-        lgS = max(lgS,((right-left)+1))
-    else:
-        while s[right] in seen:
-            seen.remove()
-            left+= 1
+def lengthOfLongestSubstring(self, s: str) -> int:
+        n = len(s)
+        maxLength = 0
+        charSet = set()
+        left = 0
+        
+        for right in range(n):
+            if s[right] not in charSet:
+                charSet.add(s[right])
+                maxLength = max(maxLength, right - left + 1)
+            else:
+                while s[right] in charSet:
+                    charSet.remove(s[left])
+                    left += 1
+                charSet.add(s[right])
+        
+        return maxLength
+
+# approach 2 :
+class Solution:
+    def lengthOfLongestSubstring(self, s: str) -> int:
+        n = len(s)
+        maxLength = 0
+        charMap = {}
+        left = 0
+        
+        for right in range(n):
+            if s[right] not in charMap or charMap[s[right]] < left:
+                charMap[s[right]] = right
+                maxLength = max(maxLength, right - left + 1)
+            else:
+                left = charMap[s[right]] + 1
+                charMap[s[right]] = right
+        
+        return maxLength
