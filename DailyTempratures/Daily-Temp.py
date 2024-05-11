309dbd12cd91ffe6607c85ee40e9308a5b14a646
hanzlikhan <khangormani79@gmail.com>
2024-05-11 15:16:09 +0500

    init

--- DailyTempratures/Daily-Temp.py
+++ DailyTempratures/Daily-Temp.py
@@ -2,9 +2,4 @@ class Solution:
     def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
         res = [0] * len(temperatures)
         stack = []
-        for i,t in enumerate(temperatures):
-            while stack and t>stack[-1][0]:
-                stackT,stackind = stack.pop()
-                res[stackind] = (i-stackind)
-            stack.append([t,i])
-        return res
+        
