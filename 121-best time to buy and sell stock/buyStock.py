15fc11a60ff8d4269168019bd9619d5d76514212
hanzlikhan <khangormani79@gmail.com>
2024-05-18 21:17:59 +0500

    init

--- 121-best time to buy and sell stock/buyStock.py
+++ 121-best time to buy and sell stock/buyStock.py
@@ -3,11 +3,11 @@ class Solution:
         left = 0
         right = 1
         max_profit = 0
-        # while right < len(prices):
-        #     currProfit = prices[right] - prices[left]
-        #     if prices[left] < prices[right]:
-        #         max_profit = max(currProfit, max_profit)
-        #     else:
-        #         left = right
-        #     right += 1
-        # return max_profit
+        while right < len(prices):
+            currProfit = prices[right] - prices[left]
+            if prices[left] < prices[right]:
+                max_profit = max(currProfit, max_profit)
+            else:
+                left = right
+            right += 1
+        return max_profit
